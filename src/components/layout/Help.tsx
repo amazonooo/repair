2b6eb8fde6afe2eaed957d4eea0440278bb0d3a9
amazonooo <garikogannisyan.9@gmail.com>
2024-07-10 20:58:57 +0300

    update link

--- src/components/layout/Help.tsx
+++ src/components/layout/Help.tsx
@@ -9,9 +9,12 @@ const Help: FC = () => {
 
   return (
 		<section className='pt-16 md:pt-24 flex flex-col items-center justify-center'>
-			<h1 style={{
-				fontFamily: 'Gunterz'
-			}} className='uppercase text-center text-black font-medium text-2xl lg:pb-14 pb-9 md:text-3xl md:leading-[48px] lg:text-[3rem] lg:leading-[64px]'>
+			<h1
+				style={{
+					fontFamily: 'Gunterz',
+				}}
+				className='uppercase text-center text-black font-medium text-2xl lg:pb-14 pb-9 md:text-3xl md:leading-[48px] lg:text-[3rem] lg:leading-[64px]'
+			>
 				Нужна помощь
 				<br /> с расчётом?
 			</h1>
@@ -29,7 +32,7 @@ const Help: FC = () => {
 					<div className='flex pt-5 gap-5'>
 						<Link
 							className='cursor-pointer hover:brightness-105 hover:shadow-md transition-all duration-300 bg-primary px-8 py-3 md:px-9 md:py-4 rounded-[3px] text-white'
-							href='tel:+7 919 951-71-76'
+							href='https://wa.me/+7(919)-951-71-76'
 						>
 							<div className='flex items-center gap-2'>
 								<FaWhatsapp className='md:text-xl text-[.875rem]' />
